31ba648a681853ccc2f63f2f484ced2146571aee
ArindamSengupta <arindam.sg.123@gmail.com>
2022-03-28 16:49:37 +0530

    add

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,1 +1,17 @@
+terraform {
+  required_version = "~> 1.0.0"
+}
+
+locals {
+  env = "dev"
+}
+
+provider "google" {
+  project = "cloudbuild-ike"
+}
+
+resource "google_service_account" "sadev" {
+  account_id   = "saaccountdev"
+  display_name = "arindamsvcd"
+}
 
--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -1,1 +1,17 @@
+terraform {
+  required_version = "~> 1.0.0"
+}
+
+locals {
+  env = "prod"
+}
+
+provider "google" {
+  project = "cloudbuild-ike"
+}
+
+resource "google_service_account" "saprod" {
+  account_id   = "saaccountprod"
+  display_name = "arindamsvcp"
+}
 
