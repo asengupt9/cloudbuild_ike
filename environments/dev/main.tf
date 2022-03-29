8d15b09ddf83534fd992cbaef4a8ae8f782c4234
ArindamSengupta <arindam.sg.123@gmail.com>
2022-03-29 16:24:03 +0530

    path

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -55,7 +55,7 @@ data "google_app_engine_default_service_account" "default" {
 data "google_client_config" "current" {}
 
 provider "cdap" {
-  host  = "${google_data_fusion_instance.datafusion_instances.service_endpoint}/api/"
+  host  = "${google_data_fusion_instance.datafusion_instances5.service_endpoint}/api/"
   token = data.google_client_config.current.access_token
   # Configuration options
 }
