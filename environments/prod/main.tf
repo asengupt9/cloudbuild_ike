3cda6d40c7603558914d6f658c7af61be0cef6d1
ArindamSengupta <arindam.sg.123@gmail.com>
2022-03-29 10:48:16 +0530

    data fusion

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -15,3 +15,24 @@ resource "google_service_account" "sadev" {
   account_id   = "saaccountdev"
   display_name = "arindamsvcd"
 }
+
+resource "google_data_fusion_instance" "extended_instance" {
+  name = "datafusion-test"
+  description = "My Data Fusion instance"
+  region = "us-central1"
+  type = "DEVELOPER"
+  enable_stackdriver_logging = true
+  enable_stackdriver_monitoring = true
+  labels = {
+    example_key = "example_value"
+  }
+  private_instance = true
+  network_config {
+    network = "default"
+    ip_allocation = "10.89.48.0/22"
+  }
+  version = "6.3.0"
+  dataproc_service_account = saaccountdev@cloudbuild-ike.iam.gserviceaccount.com
+}
+
+
